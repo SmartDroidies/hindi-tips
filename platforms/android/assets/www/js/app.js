49acd78e65ffe305790153be78a3cf2cc3b8326c
SmartDroidies <smartdroidies@gmail.com>
2015-07-24 11:03:28 +0530

    Home Page Completed

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -20,3 +20,24 @@ hindiTipsApp.config(['$routeProvider',
 		});
 	}
 ]);
+
+hindiTipsApp.config(function ($translateProvider) {
+        $translateProvider.translations('en', {
+          TITLE: 'Tamil Payanam',
+          HOME: 'Home'
+        });
+        $translateProvider.translations('hi', {
+          TITLE: 'TITLE _ HINDI',
+          HOME: 'HOME - HINDI',
+          COOKING: '\u0916\u093e\u0928\u093e \u092a\u0915\u093e\u0928\u0947',
+          REMEDY: '\u0918\u0930\u0947\u0932\u0942 \u0909\u092a\u091a\u093e\u0930',
+          FITNESS: '\u092b\u093f\u091f\u0928\u0947\u0938',
+          BEAUTY: '\u0938\u094c\u0902\u0926\u0930\u094d\u092f',
+		  HEALTH: '\u0938\u094d\u0935\u093e\u0938\u094d\u0925\u094d\u092f',
+		  HOUSE: '\u0939\u093e\u0909\u0938 \u0915\u0940\u092a\u093f\u0902\u0917'
+        });
+        $translateProvider.preferredLanguage('hi');
+      });
+
+
+/* Cooking -  - 4 | home-remedy -  3 | fitness -  6 | beauty -  2 | health -  1 | house-keeping -  */
