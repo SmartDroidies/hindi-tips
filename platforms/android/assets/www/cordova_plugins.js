4ffb0ce6ad61bf168fb71d1014aaebe81457be1f
SmartDroidies <smartdroidies@gmail.com>
2015-07-28 08:37:51 +0530

    Ad

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -11,13 +11,29 @@ module.exports = [
         "merges": [
             "window.plugins.spinnerDialog"
         ]
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.PushPlugin/www/PushNotification.js",
+        "id": "com.phonegap.plugins.PushPlugin.PushNotification",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.0.0",
-    "hu.dpal.phonegap.plugins.SpinnerDialog": "1.3.1"
+    "hu.dpal.phonegap.plugins.SpinnerDialog": "1.3.1",
+    "com.phonegap.plugins.PushPlugin": "2.5.0",
+    "org.apache.cordova.device": "0.3.0"
 }
 // BOTTOM OF METADATA
 });
